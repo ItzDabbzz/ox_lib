ed5b2d7d11ceebb299cb4c52b0636e703136432c
ItzDabbzz <itzdabbzz@gmail.com>
2025-06-12 18:56:03 -0700

    lib.hacks licenses update

--- imports/hacks/client.lua
+++ imports/hacks/client.lua
@@ -1,9 +1,10 @@
 --[[
     https://github.com/overextended/ox_lib
+    https://github.com/ItzDabbzz/ox_lib
 
     This file is licensed under LGPL-3.0 or higher <https://www.gnu.org/licenses/lgpl-3.0.en.html>
 
-    Copyright © 2025 Linden <https://github.com/thelindat>
+    Copyright © 2025 ItzDabbzz <https://github.com/ItzDabbzz>
 ]]
 
 ---@class OxHacks
